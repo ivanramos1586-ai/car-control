6d43a316c8ac23aa35a1762b9b111ed3cc723303
ivanramos1586-ai <ivanramos1586@gmail.com>
2026-04-09 15:45:12 -0300

    corrigir porta

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def delete_item(item_id):
 
 if __name__ == "__main__":
     print("🚗 Car Control rodando em http://localhost:5000")
-    app.run(debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
